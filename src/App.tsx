3b40f8b4f0f3bb427035805e88d68a8f80c832ed
Stanley <wijayastanley00@gmail.com>
2024-12-04 17:57:45 +0800

    Add header for application page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import ProjectPage from './project/page';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import AppPage from "./application_page/page";
 
 function App() {
   return (
     <div className="App">
-      <ProjectPage />
+      <AppPage />
     </div>
   );
 }
