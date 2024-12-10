4175b9ebd578179e87c62ae0061cc60e67f9e49e
Stanley <wijayastanley00@gmail.com>
2024-12-10 20:34:15 +0800

    Adjust checkmark

--- src/components/applicationForm/privacyPolicy.tsx
+++ src/components/applicationForm/privacyPolicy.tsx
@@ -1,15 +1,65 @@
+import { useState } from "react";
 import { FormHeader } from "../formSections";
 
 interface PrivacyPolicyProps {
   text: string;
+  onSelectedChange: (isSelect: boolean) => void;
+  isRead: boolean;
 }
 
-const PrivacyPolicy: React.FC<PrivacyPolicyProps> = ({ text }) => {
+const PrivacyPolicy: React.FC<PrivacyPolicyProps> = ({
+  text,
+  onSelectedChange,
+  isRead,
+}) => {
+  const handleChange = () => {
+    onSelectedChange(!isRead);
+  };
+
   return (
     <div className="w-[80%] h-[70vh] overflow-auto px-[24px] py-[48px] bg-white rounded-2xl flex-col gap-8 inline-flex overflow-auto">
       <FormHeader header="Privacy Policy" />
       <div className="p-2 border border-black overflow-auto flex-auto">
         <p className="text-xs text-justify">{text}</p>
+      </div>
+      <div className="flex gap-3">
+        <div className="flex h-6 shrink-0 items-center">
+          <div className="group grid size-4 grid-cols-1">
+            <input
+              name="check-select"
+              type="checkbox"
+              onChange={() => handleChange()}
+              className="col-start-1 row-start-1 appearance-none rounded border 
+                  border-gray-300 bg-white checked:border-[#EB5E27] 
+                  checked:bg-[#EB5E27] indeterminate:border-[#EB5E27] focus-visible:outline 
+                  focus-visible:outline-2 focus-visible:outline-offset-2 
+                  focus-visible:outline-i[#EB5E27] disabled:border-gray-300 
+                  disabled:bg-gray-100 disabled:checked:bg-gray-100 
+                  forced-colors:appearance-auto"
+            />
+            <svg
+              fill="none"
+              viewBox="0 0 14 14"
+              className="pointer-events-none col-start-1 row-start-1 size-3.5 
+                  self-center justify-self-center stroke-white 
+                  group-has-[:disabled]:stroke-gray-950/25"
+            >
+              <path
+                d="M3 8L6 11L11 3.5"
+                strokeWidth={1}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="opacity-0 group-has-[:checked]:opacity-100"
+              />
+            </svg>
+          </div>
+        </div>
+        <div className="text-sm/6">
+          <p className="font-medium text-gray-900 text-left">
+            I accept the <u>Privacy Policy</u> as well as the{" "}
+            <u>Terms & Conditions</u>.
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/pages/applicationPage.tsx
+++ src/pages/applicationPage.tsx
@@ -15,10 +15,15 @@ const ApplicationPage: React.FC<ApplicationPageProps> = ({
   privacyPolicyText,
 }) => {
   const [currentPageNum, setCurrentPageNum] = useState(1);
+  const [readPolicy, setReadPolicy] = useState(false);
 
   //temp text
   privacyPolicyText =
     "Our registered users (“Members”) share their professional identities, engage with their network, exchange knowledge and professional insights, post and view relevant content, learn and develop skills, and find business and career opportunities. Content and data on some of our Services is viewable to non-Members (“Visitors”). We use the term “Designated Countries” to refer to countries in the European Union (EU), European Economic Area (EEA), and Switzerland. Members and Visitors located in the Designated Countries or the UK can review additional information in our European Regional Privacy Notice. If you are in the “Designated Countries”, LinkedIn Ireland Unlimited Company (“LinkedIn Ireland”) will be the controller of your personal data provided to, or collected by or for, or processed in connection with our Services. If you are outside of the Designated Countries, LinkedIn Corporation will be the controller of (or business responsible for) your personal data provided to, or collected by or for, or processed in connection with our Services. As a Visitor or Member of our Services, the collection, use and sharing of your personal data is subject to this Privacy Policy and other documents referenced in this document.";
+
+  const handleSelectionChange = (isSelect: boolean) => {
+    setReadPolicy(isSelect);
+  };
 
   return (
     <div className="min-h-[100vh] bg-[#F2F1F2] flex flex-col items-center gap-8">
@@ -35,7 +40,11 @@ const ApplicationPage: React.FC<ApplicationPageProps> = ({
         ) : currentPageNum === 3 ? (
           <ScreeningQuestions />
         ) : (
-          <PrivacyPolicy text={privacyPolicyText} />
+          <PrivacyPolicy
+            text={privacyPolicyText}
+            onSelectedChange={handleSelectionChange}
+            isRead={readPolicy}
+          />
         )}
       </div>
 
@@ -73,7 +82,7 @@ const ApplicationPage: React.FC<ApplicationPageProps> = ({
             <div className="justify-start items-start flex">
               <div className="px-7 py-3 bg-[#eb5e27] rounded-[50px] justify-center items-center gap-2.5 flex">
                 <div className="text-center text-white text-base font-medium font-['Inter'] leading-normal">
-                  Submit
+                  Submit {readPolicy ? "true" : "false"}
                 </div>
               </div>
             </div>
