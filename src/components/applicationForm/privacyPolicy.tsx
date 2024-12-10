aeb199a4f9d783a9e7824d21f459657dbbf6b4c5
Stanley <wijayastanley00@gmail.com>
2024-12-10 19:59:52 +0800

    Add check select

--- src/components/applicationForm/privacyPolicy.tsx
+++ src/components/applicationForm/privacyPolicy.tsx
@@ -8,7 +8,7 @@ const PrivacyPolicy: React.FC<PrivacyPolicyProps> = ({ text }) => {
   return (
     <div className="w-[80%] h-[70vh] overflow-auto px-[24px] py-[48px] bg-white rounded-2xl flex-col gap-8 inline-flex overflow-auto">
       <FormHeader header="Privacy Policy" />
-      <div className="p-2 border border-black overflow-auto">
+      <div className="p-2 border border-black overflow-auto flex-auto">
         <p className="text-xs text-justify">{text}</p>
       </div>
     </div>
